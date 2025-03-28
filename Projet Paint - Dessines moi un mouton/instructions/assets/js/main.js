58bd90c2f14e707d0a3b3c5ce756fe87645dc179
Kalordo <yohann.leperson@gmail.com>
2025-03-28 12:32:34 +0100

    paint etape 4

--- Projet Paint - Dessines moi un mouton/instructions/assets/js/main.js
+++ Projet Paint - Dessines moi un mouton/instructions/assets/js/main.js
@@ -4,6 +4,8 @@ function selectColor(event)
     let color = colorBox.style.backgroundColor;
 
     sessionStorage.setItem("selectedColor", color);
+
+    console.log("Couleur sélectionnée :", color);
 }
 
 function getSelectedColor()
@@ -28,9 +30,24 @@ function loadPalette(palette) {
 window.addEventListener("DOMContentLoaded", function(){
     loadPalette(["#22f6f3", "#3daf7e", "#ffffff", "#ec8236", "#a9a7ee", "#ecc606", "#f783f2", "#e89e80"]);
 
-    // le code de l'étape 1 se passe ici
-    this.addEventListener("click", selectColor);
+    // le code de l'étape 2 se passe ici
+    const couleursPalette = document.querySelectorAll("header > div");
+    couleursPalette.forEach(div => {
+        div.addEventListener("click", selectColor);
+    });
 
     // le code de l'étape 3 se passe ici
-
+    const allDivsInMain = document.querySelectorAll("main div div");
+    allDivsInMain.forEach(div => {
+        div.addEventListener("click", function() {
+            let color = getSelectedColor();
+            if (color) {
+                if (div.style.backgroundColor === color) {
+                    div.style.backgroundColor = "";
+                } else {
+                    div.style.backgroundColor = color;
+                }
+            }
+        });
+    });
 });
